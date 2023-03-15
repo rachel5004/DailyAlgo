8609358877b9c717813b91ad1243c2882a37e214
rachel3486 <rachel3486@gmail.com>
2023-03-16 00:26:08 +0900

    [#92334] 다른 정답

--- src/Programmers/ReportResult.java
+++ src/Programmers/ReportResult.java
@@ -2,6 +2,7 @@ package Programmers;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,7 @@ https://school.programmers.co.kr/learn/courses/30/lessons/92334
 public class ReportResult {
 
     private static int[] getResult(String[] id_list, String[] report, int k) {
-        // 리폿당한 횟수
+
         Map<String, Integer> reportCnt = Arrays.stream(id_list)
             .collect(Collectors.toMap(id -> id, id -> 0,
                 (u, v) -> null,
@@ -43,6 +44,27 @@ public class ReportResult {
             .stream()
             .mapToInt(i -> i)
             .toArray();
+
+    }
+
+    private static int[] getAnother(String[] id_list, String[] report, int k) {
+
+        List<String> reports = Arrays.stream(report).distinct().toList();
+        HashMap<String, Integer> reportedCount = new HashMap<>();  // 누적 신고당한 횟수
+        for (String s : reports) {
+            String reportStr = s.split(" ")[1]; // 신고당한 사람
+            reportedCount.put(reportStr, reportedCount.getOrDefault(reportStr, 0) + 1);
+        }
+
+        return Arrays.stream(id_list).map(id -> {
+            List<String> reported = reports.stream()
+                                            .filter(s -> s.startsWith(id))
+                                            .map(s -> s.split(" ")[1])
+                                            .toList();
+            return reported.stream()
+                            .filter(user -> reportedCount.getOrDefault(user, 0) >= k)
+                            .count();
+        }).mapToInt(Long::intValue).toArray();
 
     }
 
